f0c9d67e04915ec17ecda4c20a0e29b89416703e
Anton Korzunov <thekashey@gmail.com>
2018-06-03 11:48:23 +1000

    add uid to SmartUID

--- src/Component.tsx
+++ src/Component.tsx
@@ -1,36 +1,47 @@
 import * as React from 'react';
+import {generateUID} from "./uid";
 
-let counter = {
-  value: 0
-};
+const createSource = () => ({
+  value: 1,
+  uid: generateUID()
+});
 
-const context = React.createContext({value: 0});
+let counter = createSource();
+
+const context = React.createContext(createSource());
 
 export interface UIDProps {
-  name?: (n: number) => string,
-  idSource?: { value: number },
-  children: (n: string) => React.ReactNode
+  name?: (n: string|number) => string,
+  idSource?: {
+    value: number,
+    uid: (item: any) => string,
+  },
+  children: (id: string, uid: (item: any) => string) => React.ReactNode
 }
 
 const getId = (source: typeof counter) => source.value++;
 
 export class UID extends React.Component<UIDProps> {
   state = {
+    quartz: this.props.idSource || counter,
     id: getId(this.props.idSource || counter)
   };
 
+  prefixId = (id: number | string) => String(this.props.name ? this.props.name(id) : id);
+
   render() {
-    const {name, children} = this.props;
-    const {id} = this.state;
-    return children(String(name ? name(id) : id))
+    const {children} = this.props;
+    const {id, quartz} = this.state;
+    return children(this.prefixId(id), (item: any) => this.prefixId(quartz.uid(item)))
   }
 }
 
-export const UIDReset: React.SFC = ({children}) => <context.Provider value={{value: 0}}>{children}</context.Provider>;
+export const UIDReset: React.SFC = ({children}) => (
+  <context.Provider value={createSource()}>{children}</context.Provider>
+);
+
 export const SmartUID: React.SFC<UIDProps> = (props) => (
   <context.Consumer>
-    {
-      (value) => <UID {...props} idSource={value}/>
-    }
+    {(value) => <UID {...props} idSource={value}/>}
   </context.Consumer>
 );
